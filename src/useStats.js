90f84e74ab4075bb4e3b7e625ab5a0b360661794
bit-bique <bique@thebitstudio.com>
2022-09-20 14:53:27 +0700

    display fps stats

---
+++ src/useStats.js
@@ -0,0 +1,16 @@
+import Stats from "stats-js";
+
+const stats = new Stats();
+stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
+document.body.appendChild(stats.dom);
+
+const useStats = () => {
+  function animate() {
+    stats.begin();
+    stats.end();
+  }
+
+  return { animate };
+};
+
+export default useStats;
